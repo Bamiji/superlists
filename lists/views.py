326e419634a76d68ce2e2e0023e5d3b961e25f21
Banji Afolabi <banji@banjislab.com>
2015-08-26 14:19:28 +0100

    Refactor home page view to use a template

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>') #if type(request) is HttpRequest:
+    return render(request, 'home.html') #if type(request) is HttpRequest:
