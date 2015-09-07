28caada69069f07f223825ee5dc0bd0f86235616
Banji Afolabi <banji@banjislab.com>
2015-09-07 13:14:51 +0100

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,14 +1,15 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(LiveServerTestCase): 
+class NewVisitorTest(StaticLiveServerTestCase): 
 
     def setUp(self): 
         self.browser = webdriver.Chrome('C:/Users/USER PC/Downloads/chromedriver_win32/chromedriver') #Firefox()
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
+        self.browser.refresh()
         self.browser.quit()
     
     def check_for_row_in_list_table(self, row_text):
@@ -105,8 +106,9 @@ class NewVisitorTest(LiveServerTestCase):
         # centered there too
         inputbox.send_keys('testing\n')
         inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta=5
+            delta=8
         )
